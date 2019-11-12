c8a6fb244e6cd43dcb6bdd6b5a7e479ebe10204c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-11-12 14:35:13 -0800

    Complete lru_cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -17,24 +17,71 @@ class LRUCache:
         self.storage = {}
 
     """
-    Retrieves the value associated with the given key. Also
-    needs to move the key-value pair to the end of the order
+    Retrieves the value associated with the given key.
+
+    Also needs to move the key-value pair to the end of the order
     such that the pair is considered most-recently used.
-    Returns the value associated with the key or None if the
-    key-value pair doesn't exist in the cache.
+
+    Returns the value associated with the key
+    or
+    None if the key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        # Check if key is storage
+        if key in self.storage:
+            # Select node
+            node = self.storage[key]
 
+            # Move selected node to front of cache
+            self.cache.move_to_front(node)
+
+            # Return nodes current value
+            return node.value[1]
+
+        return None
     """
-    Adds the given key-value pair to the cache. The newly-
-    added pair should be considered the most-recently used
-    entry in the cache. If the cache is already at max capacity
-    before this entry is added, then the oldest entry in the
-    cache needs to be removed to make room. Additionally, in the
-    case that the key already exists in the cache, we simply
-    want to overwrite the old value associated with the key with
-    the newly-specified value.
+    Adds the given key-value pair to the cache.
+
+    The newly-added pair should be considered the most-recently used
+    entry in the cache.
+
+    If the cache is already at max capacity before this entry is added,
+    then the oldest entry in the cache needs to be removed to make room.
+
+    Additionally, in the case that the key already exists in the cache,
+    we simply want to overwrite the old value associated with the key with the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.storage:
+            # Select node
+            node = self.storage[key]
+            # Assign new value to node
+            node.value = (key, value)
+            # Move to the front of cache
+            self.cache.move_to_front(node)
+            return
+
+        # Check cache capacity
+        if self.nodes is self.limit:
+            # Remove oldest item in storage
+            del self.storage[self.cache.tail.value[0]]
+            # Removes oldest item in cache
+            self.cache.remove_from_tail()
+            # Decrement amount of nodes
+            self.nodes -= 1
+
+        # Add newest item to front of cache
+        self.cache.add_to_head((key, value))
+        # Add newest item to storage
+        self.storage[key] = self.cache.head
+        # Increment current node amount
+        self.nodes += 1
+
+
+
+cache = LRUCache()
+
+cache.set('a', 'b')
+cache.set('b', 'b')
+cache.set('z', 'eb')
+cache.get('z')
