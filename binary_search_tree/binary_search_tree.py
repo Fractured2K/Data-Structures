7b6eaf154ad9ec2d9e956751bad16ffed64473e1
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-11-13 13:06:09 -0800

    Complete insert method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,7 +12,21 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # Compare value to current nodes value
+        if value < self.value:
+            # Check to see if node is a leaf
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                # Recursively call the next node until a node is inserted
+                self.left.insert(value)
+        else:
+            # Check to see if node is a leaf
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                # Recursively call the next node until a node is inserted
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
@@ -55,3 +69,13 @@ class BinarySearchTree:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+bst = BinarySearchTree(5)
+bst.insert(2)
+bst.insert(3)
+bst.insert(7)
+bst.insert(6)
+print(bst.left.value) # 2
+print(bst.right.value) # 7
+print(bst.left.right.value) # 3
+print(bst.right.left.value) #6
